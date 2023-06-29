fdfb9c35e9d34c97f779056c2583dd8daf85e1db
peacepiece7 <peacepiece7@naver.com>
2023-06-29 15:42:54 +0900

    Versioning untracked files

---
+++ src/api/handler.ts
@@ -0,0 +1,12 @@
+import type { VercelResponse, VercelRequest } from '@vercel/node'
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  })
+}
